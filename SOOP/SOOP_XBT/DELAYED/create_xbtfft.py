041c0433e4be79722f6ec4cd4926e774f18d8e66
BecCowley <rebecca.cowley@csiro.au>
2024-09-24 15:37:05 +1000

    Update flag file creation allowing for two code variables, xbt_accept_code and xbt_reject_code.

--- SOOP/SOOP_XBT/DELAYED/create_xbtfft.py
+++ SOOP/SOOP_XBT/DELAYED/create_xbtfft.py
@@ -8,16 +8,29 @@ def create_xbtfft():
     file_path = 'flag_quality_table.csv'
     df = pd.read_csv(file_path)
     # keep only the name, full_code
-    df = df[['name', 'full_code']]
+    df = df[['name', 'full_code', 'XBT_accept_code', 'XBT_reject_code']]
+    # drop the rows with NaN values in the XBT_accept_code column
+    dfa = df.dropna(subset=['XBT_accept_code'])
+    # drop the rows with NaN values in the XBT_reject_code column
+    dfr = df.dropna(subset=['XBT_reject_code'])
+
     # convert the name and full_code
-    labels = df['name'].tolist()
-    code = df['full_code'].tolist()
-    with open('xbt_fault_and_feature_type.csv', 'w') as f:
+    labels = dfa['name'].tolist()
+    full_code = dfa['full_code'].tolist()
+    with open('xbt_accept_code.csv', 'w') as f:
         # write the header
         f.write('label,code,byte_value\n')
-        for i in range(len(df)):
+        for i in range(len(dfa)):
             # write the labels, code and the binary value with a comma in between each
-            f.write(f'{convert(2**i, labels)},{code[i]},{2**i}\n')
+            f.write(f'{convert(2**i, labels)},{full_code[i]},{2**i}\n')
+    labels = dfr['name'].tolist()
+    full_code = dfr['full_code'].tolist()
+    with open('xbt_reject_code.csv', 'w') as f:
+        # write the header
+        f.write('label,code,byte_value\n')
+        for i in range(len(dfr)):
+            # write the labels, code and the binary value with a comma in between each
+            f.write(f'{convert(2**i, labels)},{full_code[i]},{2**i}\n')
     return
 
 
